5eb4e0b4bcd3427985cc14599652f50cabb5119c
Sergei <s.vasilchenko@busfor.com>
2019-07-18 23:25:38 +0300

    add decision 3.2

---
+++ 3.2.py
@@ -0,0 +1,7 @@
+def test_substring(full_string, substring):
+    # ваша реализация, напишите assert и сообщение об ошибке
+    assert substring in full_string, "expected '" + substring + "' to be substring of '"+ full_string + "'" 
+
+
+
+
