fde85e0da39c5a09733eb087fec21d30f488e2e3
Sergei <s.vasilchenko@busfor.com>
2019-07-21 22:35:25 +0300

    add abs_test

--- test_abs_project.py
+++ test_abs_project.py
@@ -1,6 +1,15 @@
-def test_abs1():
-    assert abs(-42) == 42, "Should be absolute value of a number" 
+import unittest
 
+class TestAbs(unittest.TestCase):
+    def test_abs1(self):
+        self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
+        
+    def test_abs2(self):
+        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
+        
 if __name__ == "__main__":
-    test_abs1()
-    print("All tests passed!")
+    unittest.main()
+
+#print(globals())
+
+#print('Imported:', __name__ )
